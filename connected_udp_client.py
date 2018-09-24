e7c3bbb4a2b88d0fc534448facfcb852874154c0
AnshulPatni <anshulpatni19@gmail.com>
2018-09-23 21:11:49 -0700

    Adding client & server files

---
+++ connected_udp_client.py
@@ -0,0 +1,21 @@
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+
+class Helloer(DatagramProtocol):
+
+    def startProtocol(self):
+        host = "127.0.0.1"
+        port = 8006
+
+        self.transport.connect(host, port)
+        print ("now we can only send to host %s port %d" % (host, port))
+        self.transport.write("hello".encode())
+
+    def datagramReceived(self, data, host):
+        print ("received %r from %s" % (data.decode(), host))
+
+    def connectionRefused(self):
+        print ("No one listening")
+
+reactor.listenUDP(0, Helloer())
+reactor.run()
---
+++ connected_udp_server.py
@@ -0,0 +1,14 @@
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+
+# Here's a UDP version of the simplest possible protocol
+class EchoUDP(DatagramProtocol):
+    def datagramReceived(self, datagram, address):
+        self.transport.write(datagram, address)
+
+def main():
+    reactor.listenUDP(8006, EchoUDP())
+    reactor.run()
+
+if __name__ == '__main__':
+    main()
